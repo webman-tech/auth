72a3b367de70f4633a3c5bb8e21bb203c7773b14
kriss <462679766@qq.com>
2022-05-30 20:55:00 +0800

    feature: 支持通过中间件设置当前的guard
    使得单独使用 Auth::guard() 时可以取到当前登录的用户

---
+++ src/Middleware/SetAuthGuard.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Kriss\WebmanAuth\Middleware;
+
+use Webman\Http\Request;
+use Webman\Http\Response;
+use Webman\MiddlewareInterface;
+
+/**
+ * 设置路由下的当前 auth 的 guardName
+ */
+class SetAuthGuard implements MiddlewareInterface
+{
+    public const REQUEST_GUARD_NAME = 'auth_current_guard_name';
+
+    protected string $guardName;
+
+    public function __construct(string $guardName = null)
+    {
+        $this->guardName = $guardName;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function process(Request $request, callable $handler): Response
+    {
+        if ($this->guardName) {
+            $request->{static::REQUEST_GUARD_NAME} = $this->guardName;
+        }
+
+        return $handler($request);
+    }
+}
--- src/facade/Auth.php
+++ src/facade/Auth.php
@@ -4,29 +4,39 @@ namespace Kriss\WebmanAuth\facade;
 
 use Kriss\WebmanAuth\AuthManager;
 use Kriss\WebmanAuth\Interfaces\GuardInterface;
+use Kriss\WebmanAuth\Middleware\SetAuthGuard;
 
-/**
- * @method static GuardInterface|null guard(string $name = null)
- */
 class Auth
 {
-    const REQUEST_INSTANCE_NAME = 'auth_manager__';
+    const REQUEST_AUTH_MANAGER = 'auth_manager';
 
     /**
-     * @param $name
-     * @param $arguments
+     * guard
+     * 当使用 Middleware/SetAuthGuard 后，可以获取到当前的 Guard
+     * @param string|null $name
      * @return GuardInterface|null
      */
-    public static function __callStatic($name, $arguments)
+    public static function guard(string $name = null): ?GuardInterface
+    {
+        if ($authManager = static::getAuthManager()) {
+            $name = $name ?: request()->{SetAuthGuard::REQUEST_GUARD_NAME};
+            return $authManager->guard($name);
+        }
+        return null;
+    }
+
+    /**
+     * @return AuthManager|null
+     */
+    public static function getAuthManager(): ?AuthManager
     {
         $request = request();
         if (!$request) {
             return null;
         }
-        if (!$request->{static::REQUEST_INSTANCE_NAME}) {
-            $request->{static::REQUEST_INSTANCE_NAME} = new AuthManager();
+        if (!$request->{static::REQUEST_AUTH_MANAGER}) {
+            $request->{static::REQUEST_AUTH_MANAGER} = new AuthManager();
         }
-
-        return $request->{static::REQUEST_INSTANCE_NAME}->{$name}(...$arguments);
+        return $request->{static::REQUEST_AUTH_MANAGER};
     }
 }
